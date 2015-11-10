c1c7de975b067f815b0c6cd996ab0086e75adcf0
Robert Habermann <mail@rhab.de>
2015-11-10 20:40:50 +0100

    add missing )

--- office_weather/acm/acm.py
+++ office_weather/acm/acm.py
@@ -24,7 +24,7 @@ class AirControlMini(object):
 
         if not os.path.exists(self.device):
             raise("Could not find device: " + str(self.device) +
-                  "\nMake sure it is connected (and check udev rules)."
+                  "\nMake sure it is connected (and check udev rules).")
 
         self.key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
         self.set_report = "\x00" + "".join(chr(e) for e in self.key)
